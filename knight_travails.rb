a838c252fe89bcd7c14d0078f927a5f746efcd59
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-13 16:59:27 +0200

    Add methods knight_moves and findShortestPath

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,9 +1,17 @@
 class Square
-  attr_accessor :x, :y
+  include Comparable
 
-  def initialize(x, y)
+  attr_accessor :x, :y, :path
+
+  def initialize(x, y, path = [])
     @x = x
     @y = y
+    @path = path.append([x, y])
+    p path
+  end
+
+  def <=>(other)
+    return [x, y] <=> [other.x, other.y]
   end
 
   def possibleKnightMoves
@@ -20,9 +28,42 @@ class Square
     possible_moves.map! do |coords|
       x = coords[0]
       y = coords[1]
-      Square.new(x, y) if x.between?(0,7) && y.between?(0,7)
+      Square.new(x, y, path) if x.between?(0,7) && y.between?(0,7)
     end
     possible_moves.compact!
     return possible_moves
   end
 end
+
+
+def knight_moves(start, finish)
+  start_square = Square.new(start[0], start[1])
+  finish_square = Square.new(finish[0], finish[1])
+  return findShortestPath(start_square, finish_square)
+end
+
+
+def findShortestPath(from_square, to_square)
+  visited = []
+  queue = [from_square]
+  loop do
+    temp = []
+    queue.each do |square|
+      return square if square == to_square
+
+      visited << square
+      temp << square.possibleKnightMoves
+    end
+    temp.flatten!
+    visited.each do |square|
+      temp.each do |temp_square|
+        temp.delete(temp_square) if square == temp_square
+      end
+    end
+    queue = temp
+    p queue
+  end
+end
+
+
+p knight_moves([0,0], [1,2])
